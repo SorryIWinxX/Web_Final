e36b71da69cd0e7f29a0278ac5b6501250a93382
Daniel <daniel2191937@correo.uis.edu.co>
2022-08-30 18:55:57 -0500

    finsih taller 5

--- Taller 5/src/app.js
+++ Taller 5/src/app.js
@@ -76,206 +76,6 @@ function validateUserName() {
 
 }
 
-function validateLastName() {
-
-    
-    var lastName = document.getElementById('lastname').value; //value of input
-    var lastName1 = document.getElementById('lastname'); // All element of input
-    var lastnameControl= lastName1.parentNode; // Node father to element
-
-
-
-    // least 1 character entered
-
-    if (lastName.length == 0 ) {
-        
-
-        
-        var small = lastnameControl.querySelector('small');
-        small.innerHTML = 'Last Name is required'
-        lastnameControl.className ='form-control error';
-
-
-        return false;
-    }
-
-
-    // Only letters
-
-    if (!lastName.match(/^[A-Za-z]+$/)) {
-
-        var small = lastnameControl.querySelector('small');
-        small.innerHTML = 'Write Last Name'
-        lastnameControl.className ='form-control error';
-
-        return false;
-    }
-
-    // least 3 character entered
-
-    if (lastName.length <= 3) {
-
-        var small = lastnameControl.querySelector('small');
-        small.innerHTML = 'Write Last Name'
-        lastnameControl.className ='form-control error';
-
-        return false;
-    }
-
-    // Less than 25 characters
-
-    if (lastName.length >= 25) {
-
-        var small = lastnameControl.querySelector('small');
-        small.innerHTML = 'Limit 25 characters'
-        lastnameControl.className ='form-control error';
-
-        return false;
-    }
-    
-
-    // input validate
-
-    lastnameControl.className = 'form-control success';
-    return true;
-
-
-}
-
-
-function validateAddress() {
-
-    
-    var address = document.getElementById('address').value; //value of input
-    var address1 = document.getElementById('address'); // All element of input
-    var addressControl= address1.parentNode; // Node father to element
-
-
-
-    // least 1 character entered
-
-    if (address.length == 0 ) {
-        
-
-        
-        var small = addressControl.querySelector('small');
-        small.innerHTML = 'Address is required'
-        addressControl.className ='form-control error';
-
-
-        return false;
-    }
-
-
-    // Only regex
-
-    if (!address.match(/^cll|c|av|anv|trans\b/g)) {
-
-        var small = addressControl.querySelector('small');
-        small.innerHTML = 'Start address with cll, c, av, anv or trans'
-        addressControl.className ='form-control error';
-
-        return false;
-    }
-
-    // least 3 character entered
-
-    if (address.length <= 3) {
-
-        var small = addressControl.querySelector('small');
-        small.innerHTML = 'Write Address'
-        addressControl.className ='form-control error';
-
-        return false;
-    }
-
-    // Less than 25 characters
-
-    if (address.length >= 25) {
-
-        var small = addressControl.querySelector('small');
-        small.innerHTML = 'Limit 25 characters'
-        addressControl.className ='form-control error';
-
-        return false;
-    }
-    
-
-    // input validate
-
-    addressControl.className = 'form-control success';
-    return true;
-
-
-}
-
-
-
-function validateID() {
-
-    
-    var id1 = document.getElementById('idConfig').value; //value of input
-    var id2 = document.getElementById('idConfig'); // All element of input
-    var idControl= id2.parentNode; // Node father to element
-
-
-
-    // least 1 character entered
-
-    if (id1.length == 0 ) {
-        
-
-        
-        var small = idControl.querySelector('small');
-        small.innerHTML = 'ID is required and Write only numbers'
-        idControl.className ='form-control error';
-
-
-        return false;
-    }
-
-
-    // Only digits
-
-    if (!id1.match(/\d/g)) {
-
-        var small = idControl.querySelector('small');
-        small.innerHTML = 'Write ID and only numbers'
-        idControl.className ='form-control error';
-
-        return false;
-    }
-
-    // least 3 character entered
-
-    if (id1.length <= 10) {
-
-        var small = idControl.querySelector('small');
-        small.innerHTML = 'Less 10 numbers'
-        idControl.className ='form-control error';
-
-        return false;
-    }
-
-    // Less than 25 characters
-
-    if (id1.length >= 20) {
-
-        var small = idControl.querySelector('small');
-        small.innerHTML = 'Limit 20 characters'
-        idControl.className ='form-control error';
-
-        return false;
-    }
-    
-
-    // input validate
-
-    idControl.className = 'form-control success';
-    return true;
-
-
-}
 
 function validatePassword() {
 
@@ -305,7 +105,7 @@ function validatePassword() {
     if (!password.match(strongRegex)) {
 
         var small = passwordControl.querySelector('small');
-        small.innerHTML = 'Password required 1 uppercase alphabetical character, 1 numeric character, least one special character and must be need 15 characters or longer';
+        small.innerHTML = 'Password so easy';
         passwordControl.className ='form-control error';
 
         return false;
@@ -395,3 +195,51 @@ function validateEmail() {
 
 
 }
+
+
+
+function confirmPass() {
+    
+
+    var password = document.getElementById('password').value;
+
+    console.log(password);
+
+    var confirmPassword = document.getElementById('confirmPassword').value;
+    var confirmPassword1 = document.getElementById('confirmPassword');
+    var confirmControl = confirmPassword1.parentNode.parentNode;
+
+    if (password!=confirmPassword) {
+        var small =confirmControl.querySelector('small');
+        small.innerHTML='your Password no coincide';
+        confirmControl.className='form-control error';
+    }
+
+    if(password==confirmPassword){
+        confirmControl.className='form-control success';
+    }
+
+
+}
+
+
+
+
+//Checkbox validate
+
+function clickBox() {
+    let textBox = document.getElementById('textbox');
+
+    if (document.getElementById('checkbox').checked) {
+        textBox.innerHTML = '<label for="" style="float: center;">Describe your sick</label>\
+                <div class="input-addon">\
+                    <i class="material-icons">border_color</i>\
+                </div>\
+                <textarea></textarea>'        
+    }else{
+        textBox.innerHTML = ''
+    }
+
+}
+
+
